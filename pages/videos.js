dc34bcc09d23a7c1e92aff590792f66c3188e3ea
AdrianGlez18 <adrianglezhdez18@gmail.com>
2022-03-22 23:43:00 +0000

    Pageview analytics updated

--- pages/videos.js
+++ pages/videos.js
@@ -1,4 +1,5 @@
 import { Box, Container, Heading, Image, HStack, VStack, Tag } from "@chakra-ui/react";
+import Head from "next/head";
 import Snowfall from "react-snowfall";
 import Link from "next/link";
 import styled from "@emotion/styled";
@@ -15,6 +16,18 @@ const Videos = () => {
     return (
         <Container>
             <Box display={{ md: 'flex' }}>
+            <Head>
+                    <title>Aglez Dev - Videos </title>
+                    <meta name="description" content="Section to see all my published videos about coding or projects." />
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <meta name="twitter:title" content="AglezDev" />
+                    <meta property="og:title" content="AglezDev" />
+                    <meta property="og:image" content="/og.png" />
+                    <meta property="og:description" content="Section to see all my published videos about coding or projects." />
+                    <meta property="og:url" content="https://aglez.dev" />
+                    <meta property="og:type" content="website" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                </Head>
                 <Box flexGrow={1}>
 
                     <Box bg="transparent" align="center" mt={5}>
