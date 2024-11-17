9579f9d67d29768161c2216fed81b9d10f38cf90
AdrianGlez18 <adrianglezhdez18@gmail.com>
2024-11-17 12:21:18 +0000

    Typing changes

--- components/shared/Sakura.tsx
+++ components/shared/Sakura.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import React, { useEffect, useRef } from 'react';
+import React, { MutableRefObject, useEffect, useRef } from 'react';
 import '@/styles/FallingLeaves.css';
 
 const Sakura = () => {
-    const containerRef = useRef(null);
+    const containerRef = useRef() as MutableRefObject<HTMLDivElement>;
 
     useEffect(() => {
         const container = containerRef.current;
-        const numberOfLeaves = 60; // Número de hojas que deseas mostrar
+        const numberOfLeaves = 60; 
 
         for (let i = 0; i < numberOfLeaves; i++) {
             const leaf = document.createElement('div');
@@ -17,9 +17,9 @@ const Sakura = () => {
 
             leaf.classList.add('leaf');
             leaf.style.left = `${Math.random() * 100}vw`;
-            leaf.style.animationDuration = `${Math.random() * 5 + 5}s`; // Duración aleatoria entre 5-10s
-            leaf.style.animationDelay = `${Math.random() * 5}s`; // Retraso aleatorio
-            leaf.style.transform = `rotate(${Math.random() * 360}deg)`; // Rotación inicial aleatoria
+            leaf.style.animationDuration = `${Math.random() * 5 + 5}s`;
+            leaf.style.animationDelay = `${Math.random() * 5}s`;
+            leaf.style.transform = `rotate(${Math.random() * 360}deg)`;
             container.appendChild(leaf);
         }
     }, []);
